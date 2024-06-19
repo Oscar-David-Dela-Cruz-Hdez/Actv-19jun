4290abdfa689435114040f1518bfba12690159f9
unknown <delacruzhernandezoscardavid@outlook.com>
2024-06-19 07:46:32 -0600

    php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo "mi primer php";
+echo "<br>";
+echo "mis datos";
+$nombre = "Oscar";
+$edad=23;
+$estatura=1.65;
+echo "soy... " .$nombre. " y tengo " .$edad. " años, y mido " .$estatura. " metros ";
+echo "<br>";
+var_dump($nombre);
+echo "<br>";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hola
+</body>
+</html>

0e7af21810f4d3576c9b0da1f63d5792ee5dbfbd
unknown <delacruzhernandezoscardavid@outlook.com>
2024-06-19 08:03:07 -0600

    actualizado

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
 echo "mi primer php";
 echo "<br>";
-echo "mis datos";
+echo "mis datos <br>";
 $nombre = "Oscar";
 $edad=23;
 $estatura=1.65;
 echo "soy... " .$nombre. " y tengo " .$edad. " años, y mido " .$estatura. " metros ";
 echo "<br>";
-var_dump($nombre);
+var_dump(is_int($nombre));
 echo "<br>";
-
-
+$x = 34;
+$y = 356.78;
+$z = $x+$y;
+$v = $y-$x;
+$w = $x*$y;
+$k = $y/$x;
+$r = sqrt($z);
+echo "la suma de $x y $y es $z <br>";
+echo "la resta de $y y $x es $v <br>";
+echo "la multiplicacion de $x y $y es $w <br>";
+echo "la division de $y y $x es $k <br>";
+echo "la raiz cuadrada de la suma de $z es $r <br>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
